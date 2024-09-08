9b1f95670e9180a3a030ce20998a862fb79ce948
heronlessa <heron.lessa@live.com>
2024-09-08 12:40:09 -0300

    segundocommit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css'; 
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const handleAcessar = () => {
+    if (email === 'eduardo.lino@pucpr.br' && senha === '123456') {
+      setMensagem('Acessado com sucesso!');
+    } else {
+      setMensagem('Usuário ou senha incorretos!');
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Login</h1>
+      <div className="input-group">
+        <label htmlFor="email">E-mail:</label>
+        <input 
+          type="email" 
+          id="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="senha">Senha:</label>
+        <input 
+          type="password" 
+          id="senha" 
+          value={senha} 
+          onChange={(e) => setSenha(e.target.value)} 
+        />
+      </div>
+      <button className="access-button" onClick={handleAcessar}>Acessar</button>
+      <label className="message">{mensagem}</label>
+    </div>
+  );
+}
+
+export default App;
